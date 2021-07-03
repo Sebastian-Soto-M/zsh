a2be7593edf0429c48b124f7950a4261c8115777
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-07-02 22:12:48 -0600

    zsh initial commit

---
+++ .zshrc
@@ -0,0 +1,105 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+######################
+# CUSTOM USR CONFIGS #
+######################
+DISABLE_AUTO_TITLE="true"
+DISABLE_UNTRACKED_FILES_DIRTY="true"
+# Use vim keys in tab complete menu:
+autoload -U compinit
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+compinit
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -v '^?' backward-delete-char
+
+######################
+# ZSH PLUGIN CONFIGS #
+######################
+# Path to your oh-my-zsh installation.
+export ZSH="$XDG_CONFIG_HOME"/zsh/oh-my-zsh
+ZSH_THEME="powerlevel10k/powerlevel10k"
+# TMUX configuration
+ZSH_TMUX_CONFIG="$XDG_CONFIG_HOME"/tmux/tmux.conf
+ZSH_TMUX_AUTOCONNECT=true
+ZSH_TMUX_UNICODE=true
+# ZSH_TMUX_AUTOSTART=true
+# ZSH_TMUX_AUTOQUIT=true
+# ALIAS_FINDER
+ZSH_ALIAS_FINDER_AUTOMATIC=true
+
+plugins=(
+    alias-finder
+    #asdf
+    colored-man-pages
+    fast-syntax-highlighting
+    fd
+    npm
+    node
+    fzf
+    git
+    gitignore
+    jenv
+    man
+    mvn
+    ng
+    pip
+    systemd
+    taskwarrior
+    timewarrior
+    tmux
+    virtualenvwrapper
+    web-search
+    zsh_reload
+)
+source $ZSH/oh-my-zsh.sh
+source $ZDOTDIR/alias.zsh
+
+###########################
+# TERMINAL TOOLS  CONFIGS #
+###########################
+# bat
+export BAT_THEME="Dracula"
+export  BAT_STYLE="numbers,grid"
+# asdf
+#. $ASDF_DATA_DIR/asdf.sh
+# FZF
+export FZF_BASE=/usr/bin/fzf
+export FD_OPTIONS="-pi --follow --exclude .git --exclude node_modules --exclude __pycache__"
+export FZF_DEFAULT_OPTS="
+--no-mouse --height 50% -1 --reverse --multi --inline-info
+--preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || cat {} ) 2> /dev/null | head -300'
+--preview-window='right:hidden:wrap'
+--bind='f3:execute(bat --style=numbers {} || less -f {}),f2:toggle-preview,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-a:select-all+accept'
+--color=16
+"
+export FZF_CTRL_T_OPTS="--select-1 --exit-0"
+export FZF_ALT_C_OPTS="--select-1 --exit-0"
+export FZF_DEFAULT_COMMAND="fdfind -t f -t d $FD_OPTIONS"
+export FZF_CTRl_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fdfind --type d $FD_OPTIONS"
+
+# virtualenvwrapper
+export WORKON_HOME=~/.config/virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+source $HOME/.local/bin/virtualenvwrapper.sh
+
+######################
+#  END-FILE CONFIGS  #
+######################
+export GIT_PAGER="bat"
+export EDITOR="nvim"
+export TERM="st-256color"
+export SUDO_ASKPASS="$SCRIPTS/tools/ask_pass"
+
+[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+# To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
+[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
---
+++ alias.zsh
@@ -0,0 +1,141 @@
+#git
+alias config="/usr/bin/git --git-dir=$HOME/.config/dotfiles/ --work-tree=$HOME"
+alias unv="/usr/bin/git --git-dir=$HOME/Documents/university/.unv/ \
+    --work-tree=$HOME/Documents/university"
+    # alias fm_ignore"git config core.filemode false"
+    # config config --local status.showUntrackedFiles no
+
+#file edits
+EDITOR=nvim
+
+alias e10="$EDITOR $ZDOTDIR/.p10k.zsh"
+alias ealias="$EDITOR $ZDOTDIR/alias.zsh"
+alias ebsp="$EDITOR $XDG_CONFIG_HOME/bspwm/bspwmrc"
+alias eks="$EDITOR $XDG_CONFIG_HOME/sxhkd/sxhkdrc"
+alias envrc="pushd ~/.config/nvim/"
+alias ep="$EDITOR ~/.config/picom/picom.conf"
+alias epl="$EDITOR $XDG_CONFIG_HOME/polybar/config"
+alias etmx="$EDITOR $XDG_CONFIG_HOME/tmux/tmux.conf"
+alias emypy="$EDITOR $XDG_CONFIG_HOME/mypy/config"
+alias etrc="$EDITOR $TASKRC"
+alias eze="$EDITOR $HOME/.zshenv"
+alias ezsh="$EDITOR $ZDOTDIR/.zshrc"
+alias eini="$EDITOR $XINITRC"
+alias eau="$EDITOR $XDG_DATA_HOME/dwm/autostart.sh"
+
+#folders
+jva=~/code/java
+ptn=~/code/python
+unv=~/universidad
+
+#shortcuts
+alias de="deactivate"
+alias pf="pip freeze"
+alias pfr="pip freeze > requirements.txt"
+alias te="task $1 edit"
+alias xclip="xclip -selection clipboard"
+alias clpk="colorpicker --short --preview --one-shot"
+alias o="xdg-open $1 & disown"
+alias m="ncspot"
+alias a="alias | fzf"
+alias xev="xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf \"%-3s %s\n\", \$5, \$8 }'"
+alias lstdeps="expac -S '%r/%n: %D'"
+alias pc="(rm *.orig ; rm *.rej; rm config.h) && sudo make clean install"
+alias pm="pymake"
+
+# exa
+alias exa='exa --icons'
+alias l='exa -lT --level=2'
+alias ll='exa -als type'
+alias lt='exa -T --icons'
+alias la='exa -Dxas size'
+alias ls='exa -xas type'
+
+#Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns $(pacman -Qtdq)'
+
+#merge new settings
+alias merge="xrdb -merge ~/.Xresources"
+
+# Aliases for software managment
+# pacman or pm
+alias pacman='sudo pacman --color auto'
+alias update='sudo pacman -Syyu'
+
+# yay as aur helper - updates everything
+alias pksyua="yay -Syu --noconfirm"
+alias upall="yay -Syu --noconfirm"
+
+#taskwarrior
+trd='task dtl'
+trw='task wk'
+
+alias trd="$trd"
+alias trdapi="$trd project:shopped +api"
+alias trdapp="$trd project:shopped +app"
+alias trdf="$trd | fzf"
+
+alias trw="$trw"
+alias tv="taskwarrior-tui"
+
+#timewarrior
+alias tws='timew summary :ids'
+
+#tools
+alias raid_calc="python -i ~/universidad/sistemas_operativos1/materia/src/raid_calc.py"
+alias dgd="dragon-drag-and-drop" # "dragon-drag-and-drop"
+alias rm="rmtrash"
+alias mvndb="mvnt -Dmaven.surefire.debug"
+alias mvnweb="mvn -P-webpack"
+alias fd="fdfind"
+#program alias
+alias top=bpytop
+alias vim=$EDITOR
+alias feh="feh -ZF"
+alias xlog="nvim ~/.local/share/xorg/Xorg.0.log"
+alias yarn="yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
+alias glass="picom --experimental-backends &"
+alias vims="$EDITOR -S vim-workspace"
+#functions
+ex () {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xjf $1   ;;
+            *.tar.gz)    tar xzf $1   ;;
+            *.bz2)       bunzip2 $1   ;;
+            *.rar)       unrar x $1   ;;
+            *.gz)        gunzip $1    ;;
+            *.tar)       tar xf $1    ;;
+            *.tbz2)      tar xjf $1   ;;
+            *.tgz)       tar xzf $1   ;;
+            *.zip)       unzip $1     ;;
+            *.Z)         uncompress $1;;
+            *.7z)        7z x $1      ;;
+            *.deb)       ar x $1      ;;
+            *.tar.xz)    tar xf $1    ;;
+            *)           echo "'$1' cannot be extracted via ex()" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
+
+vs() {
+    files=()
+    names=()
+    while IFS= read -r -d $'\0'; do
+        files+=("$REPLY")
+    done < <(fd . -e json "$HOME/templates" -0)
+
+    for (( i = 1; i <= ${#files[@]}; i++ )); do
+        f=$files[$i]
+        names+=$(basename $f | cut -d"." -f1)
+    done
+    sel=$(printf "%s\n" "${names[@]}" | fzf +m --prompt="Get vimspector for:")
+    echo $sel
+    for (( i = 1; i <= ${#files[@]}; i++ )); do
+        f=$files[$i]
+        n=$(basename $f | cut -d"." -f1)
+        [[ $sel == $n ]] && cp $f ".$(echo $f | cut -d '.' -f2-)"
+    done
+}
